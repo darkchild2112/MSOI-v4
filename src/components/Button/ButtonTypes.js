aa53907bf951d534346298a3f43ed53db40503d5
Ben Abbott <ben.abbott@intoglobal.com>
2023-10-04 23:01:29 +0100

    button components

---
+++ src/components/Button/ButtonTypes.js
@@ -0,0 +1,11 @@
+export const ButtonStyles = {
+    PRIMARY: 'btn-primary',
+    SECONDARY: 'btn-secondary',
+}
+
+export const ButtonTypes = {
+    BUTTON: 'button',
+    SUBMIT: 'submit',
+    RESET: 'reset',
+    LINK: 'link'
+}
