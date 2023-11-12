6bd34f3ed7d95ec14a7fcd0a9afbb03b524e4bc4
Ben Abbott <ben.abbott@intoglobal.com>
2023-11-12 16:08:14 +0000

    remove redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,13 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  redirects: {
-    '/services.htm?serviceId=1': '/services/schools',
-    '/services.htm?serviceId=2': '/services/family-mental-health',
-    '/services.htm?serviceId=3': '/services/learning-disabilities',
-    '/services.htm?serviceId=4': '/services/cognitive-assessments',
-    '/services.htm?serviceId=5': '/services/autism-assessments',
-    '/services.htm?serviceId=6': '/services/supervision-and-training'
-  },
+  // redirects: {
+  //   '/services.htm?serviceId=1': '/services/schools',
+  //   '/services.htm?serviceId=2': '/services/family-mental-health',
+  //   '/services.htm?serviceId=3': '/services/learning-disabilities',
+  //   '/services.htm?serviceId=4': '/services/cognitive-assessments',
+  //   '/services.htm?serviceId=5': '/services/autism-assessments',
+  //   '/services.htm?serviceId=6': '/services/supervision-and-training'
+  // },
   integrations: [react()]
 });
