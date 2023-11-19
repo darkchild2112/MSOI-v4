1bd3ff51b5456dc8dbcada2be290dc463931a40b
Ben Abbott <ben.abbott@intoglobal.com>
2023-11-19 11:45:48 +0000

    jodies changes + privacy policy

--- src/components/ServicesNavigation/ServicesNavigationData.js
+++ src/components/ServicesNavigation/ServicesNavigationData.js
@@ -1,8 +1,9 @@
 export const ServicesNavigationData = [
-  { link: "/services/schools", text: "Schools" },
+  { link: "/services/autism-assessments", text: "Autism Diagnostic Service" },
   { link: "/services/family-mental-health", text: "Child and Family Mental Health" },
   { link: "/services/learning-disabilities", text: "Children with Learning Disabilities" },
+  { link: "/services/schools", text: "Schools" },
   { link: "/services/cognitive-assessments", text: "Cognitive Assessments" },
-  { link: "/services/autism-assessments", text: "Autism Diagnostic Service" },
   { link: "/services/supervision-and-training", text: "Supervision & Training" },
+  
 ]
