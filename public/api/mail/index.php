5d4a9edbe4621fe327c903350f87b0cef759896d
Ben Abbott <ben.abbott@intoglobal.com>
2025-01-05 15:34:31 +0000

    check referer

--- public/api/mail/index.php
+++ public/api/mail/index.php
@@ -1,12 +1,12 @@
 <?php 
 	$apiKey = "6f384e7b-8527-48c7-bf83-62462d02b439";
-	$allowedDomain = "http://www.makingsenseofit.org.uk";
+	$allowedDomain = "https://www.makingsenseofit.org.uk";
 	header('Access-Control-Allow-Origin: *'); // Remove when live
 	$statusCode = "";
 
 	$apiKeyRequestParam = $_POST['apiKey'];
 
-	if($apiKeyRequestParam == $apiKey /*&& substr($_SERVER['HTTP_REFERER'], 0, strlen($allowedDomain)) == $allowedDomain*/)
+	if($apiKeyRequestParam == $apiKey && substr($_SERVER['HTTP_REFERER'], 0, strlen($allowedDomain)) == $allowedDomain)
 	{
 		$to = "darkchild2112@googlemail.com";
 		//$to = "info@makingsenseofit.org.uk";
