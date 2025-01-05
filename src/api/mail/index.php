220b99d22da3675d7a9812595fe7c60f97b916e3
Ben Abbott <ben.abbott@intoglobal.com>
2025-01-05 14:11:34 +0000

    mail script

---
+++ src/api/mail/index.php
@@ -0,0 +1,42 @@
+<?php 
+
+	$apiKey = "6624bb0e-bb78-42c3-83f3-1e95aba0cae0";
+	$allowedDomain = "http://www.makingsenseofit.org.uk";
+	//header('Access-Control-Allow-Origin: *'); // Remove when live
+	$statusCode = "";
+
+	if($_POST['apiKey'] == $apiKey && substr($_SERVER['HTTP_REFERER'], 0, strlen($allowedDomain)) == $allowedDomain)
+	{
+		//$to = "darkchild2112@googlemail.com";
+		$to = "info@makingsenseofit.org.uk";
+		$from = $_POST['email'];
+		$name = $_POST['name'];
+		$tel = $_POST['telephone'];
+		$message = $_POST['message'];
+		$subject = "Making Sense of It Website Enquiry";
+		
+		$newLine = "\n\n";
+
+		$message = "You have an enquiry from the Making Sense of It Website" . $newLine . "Name: " . $name . $newLine . "Email: " . $from . $newLine . "Telephone: " . $tel . $newLine . "Message: " . $message;
+
+		$headers = "From: " . $name . " <" . $from . ">";
+		
+		$success = mail($to,$subject,$message,$headers) == "1" ? true : false;
+		
+		if($success)
+		{
+			$statusCode = "200 Success";
+		}
+		else
+		{
+			$statusCode = "500 Server Error";
+		}
+	}
+	else
+	{
+		$statusCode = "400 Bad Request";
+	}
+
+	header("HTTP/1.1 " . $statusCode);
+	exit;
+?>
