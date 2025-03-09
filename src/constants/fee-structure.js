b72d43b1ca235e9d3eb3a671ed8a551424b6cb27
Ben Abbott <ben.abbott@intoglobal.com>
2025-03-09 18:16:18 +0000

    change fees

---
+++ src/constants/fee-structure.js
@@ -0,0 +1,31 @@
+const feeStructure = {
+  "NeurodevelopmentalScreenAssessment": 360,
+  "FullMultiDisciplinaryAutismDiagnosticAssessment": 2550,
+  "FullAdhdDiagnosticAssessment": 1300,
+  "CombinedAutismAndAdhdDiagnosticAssessment": 3200,
+
+  "InitialAssessment": 240,
+  "AssessmentFeedbackSession": 120,
+  "InitialAssessmentWithPreviousAssessmentsReview": 360,
+  "PostAssessmentFollowUpSessions": 120,
+  "OnGoingTherapyFollowUpSessions": 120,
+  "SchoolAppointmentWithWrittenSummaryRange": {
+    "lower": 360,
+    "higher": 480
+  },
+  "CognitiveAssessmentRange": {
+    "lower": 1100,
+    "higher": 1800
+  },
+  "HalfDayTrainingPackageRange": {
+    "lower": 600,
+    "higher": 800
+  },
+  "FullDayTrainingPackageRange": {
+    "lower": 600,
+    "higher": 800
+  },
+  "Supervision": 120,
+}
+
+export default feeStructure;
