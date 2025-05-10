056fc1e49a5dc6cc7bc25a6ab9c1ca2d14834023
Ben Abbott <ben.abbott@intoglobal.com>
2025-05-10 17:20:48 +0100

    remove period from from

--- public/api/mail/index.php
+++ public/api/mail/index.php
@@ -20,7 +20,7 @@
 
 		$message = "You have an enquiry from the Making Sense of It Website" . $newLine . "Name: " . $name . $newLine . "Email: " . $from . $newLine . "Telephone: " . $tel . $newLine . "Message: " . $message;
 
-		$headers .= "From: " . $name . " <" . $from . "> . \r\n";
+		$headers .= "From: " . $name . " <" . $from . ">\r\n";
 		$headers .= 'Reply-To: '. $from . "\r\n" ;
 		$headers .= "Organization: Sender Organization\r\n";
 		$headers .= "MIME-Version: 1.0\r\n";
