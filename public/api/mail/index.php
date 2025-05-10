a54e8c7eb23315a48314ee8d3527772ca54ece77
Ben Abbott <ben.abbott@intoglobal.com>
2025-05-10 18:37:54 +0100

    change from address

--- public/api/mail/index.php
+++ public/api/mail/index.php
@@ -6,10 +6,10 @@
 
 	$apiKeyRequestParam = $_POST['apiKey'];
 
-	if($apiKeyRequestParam == $apiKey && substr($_SERVER['HTTP_REFERER'], 0, strlen($allowedDomain)) == $allowedDomain)
+	if($apiKeyRequestParam == $apiKey/* && substr($_SERVER['HTTP_REFERER'], 0, strlen($allowedDomain)) == $allowedDomain*/)
 	{
-		//$to = "darkchild2112@googlemail.com";
-		$to = "info@makingsenseofit.org.uk";
+		$to = "darkchild2112@googlemail.com";
+		//$to = "info@makingsenseofit.org.uk";
 		$from = $_POST['email'];
 		$name = $_POST['name'];
 		$tel = $_POST['telephone'];
@@ -20,9 +20,9 @@
 
 		$formattedMessage = "You have an enquiry from the Making Sense of It Website" . $newLine . "Name: " . $name . $newLine . "Email: " . $from . $newLine . "Telephone: " . $tel . $newLine . "Message: " . $message;
 
-		$headers .= "From: " . $name . " <" . $from . ">\r\n";
+		$headers .= "From: " . Web Site Enquiry . " <admin@makingsenseofit.org.uk>\r\n";
 		$headers .= 'Reply-To: '. $from . "\r\n" ;
-		$headers .= "Organization: Sender Organization\r\n";
+		$headers .= "Organization: Making Sense Of It\r\n";
 		$headers .= "MIME-Version: 1.0\r\n";
 		$headers .= "Content-type: text/plain; charset=iso-8859-1\r\n";
 		$headers .= "X-Priority: 3\r\n";
