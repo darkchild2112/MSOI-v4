42155bd1d3bd3bae00dd2f2ad12e1e1baa7609ec
Ben Abbott <B.Abbott@autoflow.ltd.uk>
2026-08-12 23:44:38 +0100

    unique titles and descriptions on pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.makingsenseofit.org.uk',
   integrations: [react()],
   redirects: {
     '/services/autism-assessments': '/services/autism-adhd-assessments'
